48c5bfbf61c2d815ba22b46116aaa55fa36cff48
grant@bemedia.com.au <grant@bemedia.com.au>
2023-07-06 22:37:53 +0200

    add views for nfts + fts

--- frontend/src/lib/components/ContentDisplay.svelte
+++ frontend/src/lib/components/ContentDisplay.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { modalStore, type ModalSettings } from '@skeletonlabs/skeleton';
 
-	export let currentVault: any;
+	export let nfts: NFTCatalogEntry[];
+	export let fts: FTCatalogEntry[];
 	function modalComponentWithdrawNft(id: string): void {
 		const modal: ModalSettings = {
 			type: 'component',
@@ -18,41 +19,46 @@
 		};
 		modalStore.trigger(modal);
 	}
-	console.log(currentVault, 'currentVault');
+	console.log(nfts, 'nfts');
+	console.log(fts, 'fts');
 </script>
 
 <div class="flex flex-col w-full h-full pb-14">
 	<div class="flex flex-col p-4 gap-y-4 w-full">
 		<h4 class="h4 font-bold">NFTs</h4>
-		{#each currentVault as nft}
-			<div class="flex items-center justify-between border p-4 px-20">
-				<div class="flex gap-x-4 items-center">
-					<img src={nft.thumbnail} alt={nft.name} class="block h-12 w-auto" />
-					<div class="flex flex-col items-center justify-center">
-						<h4 class="h4 font-bold pb-2">{nft.name}</h4>
-						<p>{nft.description}</p>
+		{#each nfts as nftCollection}
+			<div class="flex flex-col border p-4 gap-y-2">
+				<p class="h3 font-bold">{nftCollection[0].collectionName}</p>
+				{#each nftCollection as nft}
+					<div class="flex items-center border rounded-md p-4 px-20 gap-x-6">
+						<div class="flex gap-x-4 items-center w-1/3">
+							<img src={nft.thumbnail} alt={nft.name} class="block h-12 w-auto" />
+						</div>
+						<div class="flex w-1/3">
+							<h4 class="h4 font-bold pb-2">{nft.name}</h4>
+						</div>
+						<button
+							class="btn variant-filled-primary font-bold ml-12"
+							on:click={() => modalComponentWithdrawNft(nft.id)}>Withdraw</button
+						>
 					</div>
-				</div>
-				<button
-					class="btn variant-filled-primary font-bold ml-12"
-					on:click={() => modalComponentWithdrawNft(nft.id)}>Withdraw</button
-				>
+				{/each}
 			</div>
 		{/each}
 	</div>
 	<div class="flex flex-col p-4 gap-y-4 w-full">
 		<h4 class="h4 font-bold">FTs</h4>
-		<!-- {#each currentVault.fts as ft}
-            <div class="flex items-center justify-between border px-20 p-4">
-                <div class="flex items-center justify-center">
-                    <h4 class="h4 font-bold pr-3">{ft.name}</h4>
-                    <p class="h4 font-bold pr-3">${ft.qty}</p>
-                </div>
-                <button
-                    class="btn variant-filled-primary font-bold ml-12"
-                    on:click={() => modalComponentWithdrawFt(ft.name)}>Withdraw</button
-                >
-            </div>
-        {/each} -->
+		{#each fts as ftCollection}
+			<div class="flex items-center justify-between border px-20 p-4">
+				<div class="flex items-center justify-center">
+					<h4 class="h4 font-bold pr-3">{ftCollection.token}</h4>
+					<p class="h4 font-bold pr-3">${ftCollection.balance}</p>
+				</div>
+				<button
+					class="btn variant-filled-primary font-bold ml-12"
+					on:click={() => modalComponentWithdrawFt(ftCollection.token)}>Withdraw</button
+				>
+			</div>
+		{/each}
 	</div>
 </div>
--- frontend/src/routes/profile/+page.svelte
+++ frontend/src/routes/profile/+page.svelte
@@ -7,9 +7,16 @@
 		TabGroup,
 		Tab
 	} from '@skeletonlabs/skeleton';
-	import basket from '$lib/assets/basket-icon.svg';
-	import { unauthenticate } from '@onflow/fcl';
-	import { dictionaryToArray, transactionStatus, user, usersNFTs } from '$lib/flow/stores';
+	import basketIcon from '$lib/assets/basket-icon.svg';
+
+	import {
+		dictionaryToArray,
+		transactionStatus,
+		user,
+		usersNFTs,
+		usersFTs,
+		ftDictionaryToArray
+	} from '$lib/flow/stores';
 	import ContentDisplay from '$lib/components/ContentDisplay.svelte';
 
 	function modalComponentDeposit(): void {
@@ -28,9 +35,11 @@
 	}
 
 	let tabSet: number = 0;
-	let vaults: NFTCatalogEntry[];
-	$: vaults = dictionaryToArray($usersNFTs);
-	console.log(usersNFTs, 'usersNFTs');
+	let walletNFTs: NFTCatalogEntry[];
+	let walletFTs: FTCatalogEntry[];
+	$: walletNFTs = dictionaryToArray($usersNFTs);
+	$: walletFTs = ftDictionaryToArray($usersFTs);
+	let basketCollection: any[] = [];
 </script>
 
 <div class="flex flex-col justify-center items-center">
@@ -70,33 +79,23 @@
 			<div class="flex flex-col items-center min-h-[60vh] w-full p-6">
 				<div class="w-full h-full">
 					<div class="flex w-full">
-						<div class="w-1/2 border-r-2">
+						<div class="w-1/2 border-r-4">
 							<h4 class="h3 font-bold pb-6 underline">My Wallet</h4>
-							<ContentDisplay currentVault={vaults} />
+							<ContentDisplay nfts={walletNFTs} fts={walletFTs} />
 						</div>
 						<div class="w-1/2 flex flex-col pl-6">
 							<div class="flex">
 								<TabGroup>
-									<Tab bind:group={tabSet} name="tab1" value={0}>
-										<svelte:fragment slot="lead"
-											><div class="flex items-center gap-x-3">
-												<img src={basket} alt="" class="w-10 h-10" />
-												<p class="text-2xl font-bold">1</p>
-											</div></svelte:fragment
-										>
-									</Tab>
-									<Tab bind:group={tabSet} name="tab2" value={1}
-										><div class="flex items-center gap-x-3">
-											<img src={basket} alt="" class="w-10 h-10" />
-											<p class="text-2xl font-bold">2</p>
-										</div></Tab
-									>
-									<Tab bind:group={tabSet} name="tab3" value={2}
-										><div class="flex items-center gap-x-3">
-											<img src={basket} alt="" class="w-10 h-10" />
-											<p class="text-2xl font-bold">3</p>
-										</div></Tab
-									>
+									{#each basketCollection as basket}
+										<Tab bind:group={tabSet} name="tab1" value={basket.indexOf}>
+											<svelte:fragment
+												><div class="flex items-center gap-x-3">
+													<img src={basketIcon} alt="" class="w-10 h-10" />
+													<p class="text-2xl font-bold">{basket.indexOf}</p>
+												</div></svelte:fragment
+											>
+										</Tab>
+									{/each}
 								</TabGroup>
 								<div class="ml-2 pt-2">
 									<button
@@ -109,14 +108,11 @@
 							</div>
 							<!-- Tab Panels --->
 							<div>
-								<ContentDisplay currentVault={vaults} />
-								{#if tabSet === 0}
-									(Basket 1 contents)
-								{:else if tabSet === 1}
-									(Basket 2 contents)
-								{:else if tabSet === 2}
-									(Basket 3 contents)
-								{/if}
+								<!-- {#each basketCollection as basket}
+									{#if tabSet === basket.position}
+										<ContentDisplay currentVault={basket} />
+									{/if}
+								{/each} -->
 							</div>
 						</div>
 					</div>
